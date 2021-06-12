b8160b060b51dbc1252b4624a98520375ace0024
YiFei2 <fongyifei1999@gmail.com>
2021-06-13 00:32:11 +0800

    AudioManager addition

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
